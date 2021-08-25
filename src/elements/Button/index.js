6d023d0c1012c1f89d7e73e28176de03f0b2c95f
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2021-08-25 19:27:01 +0700

    Create Header component and put to arrange LandingPage

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -38,7 +38,7 @@ export default function Button(props) {
           className={className.join(" ")}
           style={props.style}
           target={props.target === "_blank" ? "_blank" : undefined}
-          rel={props.target === "_blank" ? "noopener norefferer" : undefined}
+          rel={props.target === "_blank" ? "noopener noreferrer" : undefined}
         >
           {props.children}
         </a>
