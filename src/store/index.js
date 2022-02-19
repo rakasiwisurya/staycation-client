b5ec57e2ac92c40e81cc8a06e6017bd280512aed
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2022-02-19 07:48:18 +0700

    done setup public for production and disabled redux on production mode

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,8 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__
+    process.env.NODE_ENV === "development" &&
+      window.__REDUX_DEVTOOLS_EXTENSION__
       ? window.__REDUX_DEVTOOLS_EXTENSION__()
       : (f) => f
   )
