802d62439089b2d61f65260dc82cbd3de85b7925
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2022-02-19 17:41:59 +0700

    done responsive mobile on landing page

--- src/parts/Categories.js
+++ src/parts/Categories.js
@@ -49,44 +49,6 @@ export default function Categories({ data }) {
               </div>
             ))}
           </div>
-          {/* <div className="container-grid">
-            {category.itemId.map((item, index2) => (
-              <div
-                className="item column-3 row-1"
-                key={`category-${index1}-item-${index2}`}
-              >
-                <Fade direction={"up"} delay={300 * index2}>
-                  <div className="card">
-                    {item.isPopular && (
-                      <div className="tag">
-                        Popular{" "}
-                        <span className="font-weight-light">Choice</span>
-                      </div>
-                    )}
-                    <figure className="img-wrapper" style={{ height: 180 }}>
-                      <img
-                        src={item.imageId[0].imageUrl}
-                        alt={item.imageId[0].imageUrl}
-                        className="img-cover"
-                      />
-                    </figure>
-                    <div className="meta-wrapper">
-                      <Button
-                        type="link"
-                        href={`/properties/${item._id}`}
-                        className="stretched-link d-block text-gray-800"
-                      >
-                        <h5 className="h4">{item.title}</h5>
-                      </Button>
-                      <span className="text-gray-500">
-                        {item.city}, {item.country}
-                      </span>
-                    </div>
-                  </div>
-                </Fade>
-              </div>
-            ))}
-          </div> */}
         </Fade>
       </section>
     );
